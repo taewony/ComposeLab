be3d4a0b5222fb10052302614e80bf05ce3f707b
taewony <engchat@gmail.com>
2025-07-04 16:13:27 +0900

    some chg

--- app/src/main/java/com/example/composelab/MainActivity.kt
+++ app/src/main/java/com/example/composelab/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -37,6 +39,24 @@ class MainActivity : ComponentActivity() {
 
             }
         }
+    }
+}
+
+@Composable
+fun Test() {
+    Box(modifier = Modifier.fillMaxSize().background(color = Color.Yellow), contentAlignment = Alignment.Center) {
+        Text("It's me!")
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopEnd) {
+            Text("It's me~~~~~~~~~~~~~~")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TestPreview() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Test()
     }
 }
 
