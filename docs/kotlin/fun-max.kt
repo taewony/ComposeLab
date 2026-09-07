8ea7351722e867413645261e388fdb16ad5bb61c
taewony <engchat@gmail.com>
2026-09-07 15:58:45 +0900

    kotlin sample codes

---
+++ docs/kotlin/fun-max.kt
@@ -0,0 +1,10 @@
+fun max(a: Int, b: Int) = if (a>b) a else b
+
+fun main() {
+    println("max (9,5) = ${max(9, 5)} !")
+    
+    val fruitList = mutableListOf("apple", "banana")
+    
+  	fruitList.add("mellon")
+    println("fruitList = $fruitList")
+}
